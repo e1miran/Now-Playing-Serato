c931eed4addf8648b3e14e88b055e7470ed9bbc6
Allen Wittenauer <aw@effectivemachines.com>
2021-10-24 21:45:36 -0700

    Rework how binaries are built (#280)

---
+++ builder.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -ex
+
+SYSTEM=$1
+
+if [[ -z "${SYSTEM}" ]]; then
+  echo "Provide extra requirements"
+  exit 1
+fi
+
+case "${SYSTEM}" in
+  windows)
+    PYTHON=python
+    ;;
+  macosx)
+    PYTHON=python3
+    "${PYTHON}" -m venv /tmp/venv
+    # shellcheck disable=SC1091
+    source /tmp/venv/bin/activate
+  ;;
+esac
+
+rm -rf build dist || true
+
+"${PYTHON}" -m pip install --upgrade pip
+pip install -r requirements.txt
+pip install -r requirements-"${SYSTEM}".txt
+"${PYTHON}" setup.py build
+mv nowplaying/version.py nowplaying/version.py.old
+mv build/lib/nowplaying/version.py nowplaying/version.py
+pyinstaller NowPlaying.spec
+cp -p CHANGELOG* README* LICENSE.txt NOTICE.txt dist
+mv dist NowPlaying-"${SYSTEM}"
+mv nowplaying/version.py.old nowplaying/version.py
+
+if [[ "${SYSTEM}" == "macosx" ]]; then
+  rm -rf NowPlaying-macosx/NowPlaying || true
+  zip -r NowPlaying-macosx.zip NowPlaying-macosx
+fi
