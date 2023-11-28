00b9a95118a11dbf6f5b1cc3e99a075fcb288f54
Allen Wittenauer <aw@effectivemachines.com>
2023-11-28 13:24:20 -0800

    Make builder.sh more friendly (#969)

--- builder.sh
+++ builder.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 SYSTEM=$1
 VERSION=$(git describe --tags)
 DISTDIR=NowPlaying-"${VERSION}-${SYSTEM}"
+UNAMESYS=$(uname -s)
 
 if [[ -z "${SYSTEM}" ]]; then
-  echo "Provide extra requirements: 'windows' or 'macosx' "
-  exit 1
+  case "${UNAMESYS}" in
+    Darwin)
+      SYSTEM=macosx
+      ;;
+    MINGW*)
+      SYSTEM=windows
+      ;;
+    *)
+      ;;
+  esac
 fi
 
 case "${SYSTEM}" in
@@ -23,7 +32,13 @@ case "${SYSTEM}" in
     ;;
 esac
 
-PYTHON_VERSION=$("${PYTHON}" --version)
+PYTHONBIN=$(command -v "${PYTHON}")
+echo "*****"
+echo "* Building on ${SYSTEM} / ${UNAMESYS}"
+echo "* Using ${PYTHONBIN}"
+echo "****"
+
+PYTHON_VERSION=$("${PYTHONBIN}" --version)
 PYTHON_VERSION=${PYTHON_VERSION#* }
 IFS="." read -ra PY_VERSION <<< "${PYTHON_VERSION}"
 
@@ -33,24 +48,77 @@ if [[ ${PY_VERSION[0]} -ne 3 && ${PY_VERSION[1]} -ne 10 ]]; then
 fi
 
 case "${SYSTEM}" in
-  macosx)
-    "${PYTHON}" -m venv /tmp/venv
+  windows)
+    echo "*****"
+    echo "* Building a virtual environment"
+    echo "****"
+    rm -rf "${TMP}/build-venv" || true
+    "${PYTHONBIN}" -m venv "${TMP}/build-venv"
     # shellcheck disable=SC1091
-    source /tmp/venv/bin/activate
+    source "${TMP}/build-venv/scripts/activate"
     ;;
   *)
+    echo "*****"
+    echo "* Building a virtual environment"
+    echo "****"
+    rm -rf /tmp/build-venv || true
+    "${PYTHONBIN}" -m venv /tmp/build-venv
+    # shellcheck disable=SC1091
+    source /tmp/build-venv/bin/activate
     ;;
 esac
 
-
+PYTHONBIN=$(command -v "${PYTHON}")
+PYTHONBINDIR=$(dirname "${PYTHONBIN}")
 
 rm -rf build dist || true
 
-"${PYTHON}" -m pip install --upgrade pip
-pip install ".[dev,osspecials]"
-"${PYTHON}"  setupnltk.py
-pyside6-rcc nowplaying/resources/settings.qrc > nowplaying/qtrc.py
-pyinstaller NowPlaying.spec
+echo "*****"
+echo "* Upgrading pip"
+echo "****"
+
+"${PYTHONBIN}" -m pip install --upgrade pip
+
+echo "*****"
+echo "* Installing dependencies"
+echo "****"
+
+"${PYTHONBIN}" -m pip install ".[dev,osspecials]"
+
+echo "*****"
+echo "* Setting up NLTK"
+echo "****"
+
+"${PYTHONBIN}"  setupnltk.py
+
+echo "*****"
+echo "* Compiling Qt resources"
+echo "****"
+
+if [[ -x "${PYTHONBINDIR}/pyside6-rcc" ]]; then
+  "${PYTHONBINDIR}/pyside6-rcc" nowplaying/resources/settings.qrc > nowplaying/qtrc.py
+else
+  rcc=$(command -v pyside6-rcc)
+  echo "Using ${rcc}"
+  pyside6-rcc nowplaying/resources/settings.qrc > nowplaying/qtrc.py
+fi
+
+echo "*****"
+echo "* Making binary with PyInstaller "
+echo "****"
+
+if [[ -x "${PYTHONBINDIR}/pyinstaller" ]]; then
+  "${PYTHONBINDIR}/pyinstaller" NowPlaying.spec
+else
+  rcc=$(command -v pyinstaller)
+  echo "Using ${rcc}"
+  pyinstaller NowPlaying.spec
+fi
+
+echo "*****"
+echo "* Cleanup "
+echo "****"
+
 cp -p CHANGELOG* README* LICENSE.txt NOTICE.txt dist
 mv dist "${DISTDIR}"
 
